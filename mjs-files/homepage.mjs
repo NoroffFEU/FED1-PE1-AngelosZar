eed283d0d1470c6e2771f37d972ff4da03352a7a
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-04-30 18:21:20 +0200

    main function
    Creation of main function and a draft of the function that i do need to create for the homepage.

--- mjs-files/homepage.mjs
+++ mjs-files/homepage.mjs
@@ -6,6 +6,31 @@ import { singlePost } from './common.mjs';
 import { singlePostId } from './common.mjs';
 
 //all posts
-fetchData(allArticles);
+// fetchData(allArticles);
 // single post by id
-fetchData(baseApiUrl, singlePostId);
+// fetchData(baseApiUrl, singlePostId);
+
+//
+// function to display a grid card on
+// Variables from Grid
+// const gridContainer = document.querySelector('.cards-container');
+// const gridCard = document.querySelector('.grid-card"');
+//
+//
+//
+async function main() {
+  // 1. check if logged in
+  // 2. check if user is admin
+
+  // 3. Async load api data for
+  try {
+    const { data: posts } = await fetchData(allArticles);
+    // functions to render sections here
+    //  - hero section
+    //  - picks of the day //carousel section/
+    //  - most recent section / grid of min 12 cards
+  } catch (error) {
+    console.log('Problem loading the content');
+  }
+}
+main();
