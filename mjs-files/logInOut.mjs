39bdde7c4cea4c561ad66fd0a380d5e14d4dd248
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-05-02 17:30:32 +0200

    Create a user function
    Successfully created a function to create a user .
    It has been tested on https://reqres.in/
    as i was constantly getting back an error/400 code  from the noroff api .Which is understandable due to false credentials

--- mjs-files/logInOut.mjs
+++ mjs-files/logInOut.mjs
@@ -5,40 +5,47 @@ import { baseApiUrl } from './common.mjs';
 
 const registerEndPoint = '/auth/register';
 const registerUrl = `${baseApiUrl}${registerEndPoint}`;
-
 // Log in user variables
 const logInUserName = document.querySelector('#email-input-log_in').value;
 const logInUserEmail = document.querySelector('#password-input').value;
 const logInBtn = document.querySelector('#btn-log-in');
 logInBtn.addEventListener('click', () => {
+  preventDefault();
   console.log(logInUserName, logInUserEmail);
 });
 
-//
-//
-//
-// const axios = require('axios');
-//
-
-async function registerUser(RegisteringUser) {
-  //   const axios = require('axios');
-  try {
-    const response = await axios.post(
-      `${baseApiUrl}${registerEndPoint}`,
-      RegisteringUser
-    );
-    return response.data;
-  } catch (error) {
-    console.error('Error creating user:', error);
-    return null; // You can handle errors differently based on your requirements
-  }
-}
-//
 // Take later info from user input
-let RegisteringUser = {
+let registeringUser = {
   name: 'my_username',
   email: 'first.last@stud.noroff.no',
   password: 'UzI1NiIsInR5cCI',
 };
-registerUser(RegisteringUser);
-console.log('efwefw');
+
+// api test platform
+const reqtest = 'https://reqres.in/api/register';
+const reqUser = {
+  email: 'eve.holt@reqres.in',
+  password: 'pistol',
+};
+
+async function registerUser(user, api) {
+  try {
+    // const response = await fetch.(`${baseApiUrl}${registerEndPoint}`);
+    const res = await fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    const responseData = await res.json();
+    console.log(responseData);
+    // prompt('Congratulation \nNew user was registered');
+    return responseData;
+  } catch (error) {
+    console.error(`Error creating user:, ${error.message}`);
+    return null; // You can handle errors differently based on your requirements
+  }
+}
+//
+// registerUser(reqUser, reqtest);
