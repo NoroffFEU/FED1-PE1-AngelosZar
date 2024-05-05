b3d9a067d276b9fe585cbdefdda140e233acbf50
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-05-05 11:08:52 +0200

    Delete function
    Delete function is now wokring properly. i just have create a function to get post by id and incorporate it into the delete function

--- mjs-files/deletePost.mjs
+++ mjs-files/deletePost.mjs
@@ -1,22 +1,23 @@
 // import { deletePost } from './common.mjs';
 const admin = 'angZar';
 import { fetchData } from './common.mjs';
-const singlePostId = '00a949dd-8cdf-4b1a-a70c-96701e02b34b';
+const singlePostId = 'e3d24557-d108-44f3-a68e-f5999bc36a1f';
 import { baseApiUrl } from './common.mjs';
-const deletePostUrl = `${baseApiUrl}/blog/posts/${admin}/${singlePostId}`;
-const token = localStorage.getItem('token');
-const bearerToken = `Bearer ${token}`;
+// const deletePostUrl = `${baseApiUrl}/blog/posts/${admin}/${singlePostId}`;
+// const token = localStorage.getItem('token');
+// const bearerToken = `Bearer ${token}`;
 const username = localStorage.getItem('name');
-console.log(typeof token);
-console.log(typeof bearerToken);
+// console.log(typeof token);
+// console.log(typeof bearerToken);
 // document.addEventListener('DOMContentLoaded', () => {});
 
 // testing reqres succesfull
 // const testDeletePost = 'https://reqres.in/api/users/2';
-
-const deletePost = async function (token, postId) {
-  const admin = 'angZar';
-  const deleteApiUrl = `${baseApiUrl}/blog/posts/${admin}/${postId}`;
+console.log('fine');
+const deletePost = async function (name, postId) {
+  // const name = localStorage.getItem('name');
+  const token = localStorage.getItem('accessToken');
+  const deleteApiUrl = `https://v2.api.noroff.dev/blog/posts/${name}/${postId}`;
   try {
     const res = await fetch(deleteApiUrl, {
       method: 'DELETE',
@@ -30,12 +31,12 @@ const deletePost = async function (token, postId) {
       console.log('Post was successfully deleted');
       alert('Post was successfully deleted');
     }
-    console.log('data', data);
-    return data;
+    // console.log('data', data);
+    // return data;
   } catch (error) {
     console.log(`${error.message}`);
     throw new Error(`error) ${error.message}`);
   }
 };
 
-// deletePost(token, singlePostId);
+// deletePost(username, singlePostId);
