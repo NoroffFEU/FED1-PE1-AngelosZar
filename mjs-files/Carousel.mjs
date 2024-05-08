117e033d6f3a56f4e7f08479836cf3e0df5440e2
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-05-08 18:24:14 +0200

    Carousel dots
    Create the dots with insertAdjacentHTML.
    Add an event listener to the dots div .on each div an event target and use the moveslide fuction to redirect it on the current index /dot .image

--- mjs-files/Carousel.mjs
+++ mjs-files/Carousel.mjs
@@ -3,13 +3,10 @@ const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const sliderBtnLeft = document.querySelector('.slider-btn-left');
 const sliderBtnRight = document.querySelector('.slider-btn-right');
-
+const dotContainer = document.querySelector('.dots');
 // slider.style.transform = 'scale(0.2)';
 // slider.style.overflow = 'visible';
 
-slides.forEach(
-  (slide, index) => (slide.style.transform = `translateX(${index * 100}%)`)
-);
 // slides
 let currentSlide = 0;
 // use this variable for the api as well to keep slides at three
@@ -31,3 +28,29 @@ const prevSlide = function () {
 };
 sliderBtnRight.addEventListener('click', nextSlide);
 sliderBtnLeft.addEventListener('click', prevSlide);
+
+// keyboard events fro slider
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'ArrowRight') nextSlide();
+  if (e.key === 'ArrowLeft') prevSlide();
+});
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    moveSlide((currentSlide = slide));
+    console.log('DOTS', e.target);
+  }
+});
+moveSlide(0);
+createDots();
