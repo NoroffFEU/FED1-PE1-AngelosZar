429637499a8821d6125ae48726e100ceea206e0f
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-05-08 16:30:09 +0200

    Slider carousel
    Created a function that moves 100% of the width of the card to x axis .
    And a turnery operator  in Another function to add some logic when its the last card to go back to index 0 .

---
+++ mjs-files/Carousel.mjs
@@ -0,0 +1,29 @@
+//
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const sliderBtnLeft = document.querySelector('.slider-btn-left');
+const sliderBtnRight = document.querySelector('.slider-btn-right');
+
+slider.style.transform = 'scale(0.2)';
+slider.style.overflow = 'visible';
+
+slides.forEach(
+  (slide, index) => (slide.style.transform = `translateX(${index * 100}%)`)
+);
+// slides
+let currentSlide = 0;
+// use this variable for the api as well to keep slides at three
+const maxSlideNum = slides.length;
+//
+const moveSlide = function () {
+  slides.forEach(
+    (slide, index) =>
+      (slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`)
+  );
+};
+const nextSlide = function () {
+  currentSlide === maxSlideNum - 1 ? (currentSlide = 0) : currentSlide++;
+  moveSlide();
+};
+
+sliderBtnRight.addEventListener('click', nextSlide);
