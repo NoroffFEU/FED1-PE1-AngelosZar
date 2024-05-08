93be229ab2ae5e72ea5c113483ac2b291356a947
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-05-08 16:49:21 +0200

    Previous btn on carousel
    Created a previous btn function similar to next slide just decreasing the index .

--- mjs-files/Carousel.mjs
+++ mjs-files/Carousel.mjs
@@ -4,8 +4,8 @@ const slides = document.querySelectorAll('.slide');
 const sliderBtnLeft = document.querySelector('.slider-btn-left');
 const sliderBtnRight = document.querySelector('.slider-btn-right');
 
-slider.style.transform = 'scale(0.2)';
-slider.style.overflow = 'visible';
+// slider.style.transform = 'scale(0.2)';
+// slider.style.overflow = 'visible';
 
 slides.forEach(
   (slide, index) => (slide.style.transform = `translateX(${index * 100}%)`)
@@ -15,7 +15,7 @@ let currentSlide = 0;
 // use this variable for the api as well to keep slides at three
 const maxSlideNum = slides.length;
 //
-const moveSlide = function () {
+const moveSlide = function (slide) {
   slides.forEach(
     (slide, index) =>
       (slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`)
@@ -23,7 +23,11 @@ const moveSlide = function () {
 };
 const nextSlide = function () {
   currentSlide === maxSlideNum - 1 ? (currentSlide = 0) : currentSlide++;
-  moveSlide();
+  moveSlide(currentSlide);
 };
-
+const prevSlide = function () {
+  currentSlide === 0 ? (currentSlide = maxSlideNum - 1) : currentSlide--;
+  moveSlide(currentSlide);
+};
 sliderBtnRight.addEventListener('click', nextSlide);
+sliderBtnLeft.addEventListener('click', prevSlide);
