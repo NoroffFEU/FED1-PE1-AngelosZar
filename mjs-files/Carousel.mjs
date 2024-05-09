cf9db9bbb0e689b746ca6ff1f350d927cae2d20c
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-05-09 18:07:42 +0200

    slider eleemet
    Two functions created .
    createSlide to generate the content of each slide.
    Populate slider to fetch the data set limit to three posts and nest the previous function in .as well as the rest functions necessary for the slider.

--- mjs-files/Carousel.mjs
+++ mjs-files/Carousel.mjs
@@ -1,4 +1,6 @@
 //
+import { fetchData } from './common.mjs';
+import { allArticles } from './common.mjs';
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const sliderBtnLeft = document.querySelector('.slider-btn-left');
@@ -8,23 +10,28 @@ const dotContainer = document.querySelector('.dots');
 let currentSlide = 0;
 // use this variable for the api as well to keep slides at three
 const maxSlideNum = slides.length;
+// const maxSlideNum = 3;
 //
+// Move the slides to the left or right
 const moveSlide = function (slide) {
   slides.forEach(
     (slide, index) =>
       (slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`)
   );
 };
+// move to the next slide
 const nextSlide = function () {
   currentSlide === maxSlideNum - 1 ? (currentSlide = 0) : currentSlide++;
   moveSlide(currentSlide);
   syncDots(currentSlide);
 };
+// move to the previous slide
 const prevSlide = function () {
   currentSlide === 0 ? (currentSlide = maxSlideNum - 1) : currentSlide--;
   moveSlide(currentSlide);
   syncDots(currentSlide);
 };
+// event listeners on the buttons
 sliderBtnRight.addEventListener('click', nextSlide);
 sliderBtnLeft.addEventListener('click', prevSlide);
 
@@ -34,7 +41,7 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'ArrowRight') nextSlide();
   if (e.key === 'ArrowLeft') prevSlide();
 });
-
+// dynamically create the dots
 const createDots = function () {
   slides.forEach(function (_, i) {
     dotContainer.insertAdjacentHTML(
@@ -43,6 +50,7 @@ const createDots = function () {
     );
   });
 };
+// synchronize the dots with the slides
 const syncDots = function (slide) {
   // remove active class from all dots
   document.querySelectorAll('.dots__dot').forEach(dot => {
@@ -53,6 +61,7 @@ const syncDots = function (slide) {
     .querySelector(`.dots__dot[data-slide="${slide}"]`)
     .classList.add('dots__dot--active');
 };
+// make dots clickable and move to the slide
 dotContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('dots__dot')) {
     const { slide } = e.target.dataset;
@@ -61,10 +70,42 @@ dotContainer.addEventListener('click', function (e) {
     console.log('DOTS', e.target);
   }
 });
-
+//
+// create slide with adjacent html
+const createSlide = function (post) {
+  const slide = document.createElement('div');
+  slide.classList.add('slide');
+  // not sure if ts will accept this
+  // slide.classList.add(`'slide--${post[i]}'`);
+  slide.insertAdjacentHTML = `
+<img src="${post.media.url}" alt="${post.media.alt}" class="slide__img" />
+`;
+  slider.appendChild(slide);
+};
+// fetch and populate the slides
+const populateSlider = async function () {
+  try {
+    const { data: posts } = await fetchData(allArticles);
+    console.log('Posts:', posts);
+    for (let i = 0; i < 3; i++) {
+      console.log(posts[i]);
+      // createSlide(posts[i]);
+      // createSlide(posts;
+    }
+  } catch (error) {
+    console.error(error);
+    console.log('Problem loading the content');
+  }
+  // moveSlide(0);
+  // createDots();
+  // clickedPost();
+  // clickedPost(posts[i]);
+};
+//
 const initCarousel = function () {
   moveSlide(0);
   createDots();
   syncDots(0);
 };
 initCarousel();
+populateSlider();
